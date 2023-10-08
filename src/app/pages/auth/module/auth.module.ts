9396bcd77d045a28bbf51cefa62b7430d883b17e
emmat_york <andrei.filimonchyk@gmail.com>
2023-10-09 01:14:39 +0200

    auth component set up

--- src/app/pages/auth/module/auth.module.ts
+++ src/app/pages/auth/module/auth.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from "../components/sign-in/sign-in.component";
 import { SignUpComponent } from "../components/sign-up/sign-up.component";
+import { AuthComponent } from "../auth.component";
+import { RouterOutlet } from "@angular/router";
 
 @NgModule({
-    declarations: [SignUpComponent, SignInComponent],
+    declarations: [AuthComponent, SignUpComponent, SignInComponent],
+    imports: [RouterOutlet],
 })
 export class AuthModule {}
