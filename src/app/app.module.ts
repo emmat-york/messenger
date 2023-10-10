31f497e2c346025261a0d1f5bb853ecee1773610
emmat_york <andrei.filimonchyk@gmail.com>
2023-10-10 23:15:56 +0200

    i18n: i18n set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { appStore } from "./store/store";
 import { AuthEffects } from "./store/effects/auth.effects";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { LetDirective } from "@ngrx/component";
 import { AuthModule } from "./pages/auth/module/auth.module";
 import { MessengerModule } from "./pages/messanger/module/messenger.module";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { Languages } from "./shared/enums/languages.enums";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +30,14 @@ import { MessengerModule } from "./pages/messanger/module/messenger.module";
     LetDirective,
     AuthModule,
     MessengerModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: Languages.En,
+    })
   ],
   bootstrap: [AppComponent],
 })
