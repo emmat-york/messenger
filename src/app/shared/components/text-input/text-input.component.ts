70cc978add8d2c41f29dffedec33d5e44c5a532d
emmat_york <andrei.filimonchyk@gmail.com>
2023-10-11 21:03:48 +0200

    text-input component: set TextInputComponent

---
+++ src/app/shared/components/text-input/text-input.component.ts
@@ -0,0 +1,55 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  standalone: true,
+  templateUrl: 'text-input.component.html',
+  styleUrls: ['text-input.component.scss'],
+  imports: [FormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: TextInputComponent,
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextInputComponent implements ControlValueAccessor {
+  @Input() id!: string;
+  value = '';
+  disabled = false;
+
+  onChange(value: string): void {}
+  onBlur(): void {}
+
+  onInputChange(value: string): void {
+    this.value = value;
+    this.onChange(value);
+  }
+
+  onInputBlur(): void {
+    this.onBlur();
+  }
+
+  writeValue(value: string | null): void {
+    this.value = value || '';
+  }
+
+  setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+
+  registerOnChange(fn: (_: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onBlur = fn;
+  }
+}
