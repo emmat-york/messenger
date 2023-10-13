060e4d7e988c1f4ff504ab8cbf6cdca806dfbbc6
emmat_york <andrei.filimonchyk@gmail.com>
2023-10-14 01:02:27 +0200

    SignInComponent update naming

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -33,7 +33,7 @@ export class SignInComponent
   signInKey = SignInKey;
 
   constructor(
-    private translateService: TranslateService,
+    private translate: TranslateService,
     private authFacade: AuthFacade,
   ) {
     super();
