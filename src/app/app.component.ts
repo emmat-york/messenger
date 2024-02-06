d7bc53337447b2b5d93d7f5207e90cf901477c25
afilimonchyk <afilimonchyk@spribe.co>
2024-02-06 10:54:38 +0100

    chat: update theme subscription approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, DestroyRef, Inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {SettingsFacade} from "./store/settings/settings.facade";
-import {NgClass} from "@angular/common";
+import {DOCUMENT, NgClass} from "@angular/common";
 import {PushPipe} from "@ngrx/component";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,20 @@ import {PushPipe} from "@ngrx/component";
   styleUrl: 'app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  theme$ = this.settingsFacade.theme$;
+export class AppComponent implements OnInit {
+  constructor(
+    @Inject(DestroyRef) private readonly destroyRef: DestroyRef,
+    @Inject(DOCUMENT) private readonly document: Document,
+    private readonly settingsFacade: SettingsFacade,
+  ) {}
 
-  constructor(private readonly settingsFacade: SettingsFacade) {}
+  ngOnInit(): void {
+    this.subscribeToTheme();
+  }
+
+  private subscribeToTheme(): void {
+    this.settingsFacade.theme$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((theme) => {
+      this.document.body.setAttribute('class', theme);
+    });
+  }
 }
