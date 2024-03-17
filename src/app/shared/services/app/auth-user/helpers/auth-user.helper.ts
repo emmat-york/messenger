e58c3f587436c6351a0d1054e60e2b144e2b1b92
emmat_york <andrei.filimonchyk@gmail.com>
2024-03-17 17:24:35 +0100

    chat: login logic 3

--- src/app/shared/services/app/auth-user/helpers/auth-user.helper.ts
+++ src/app/shared/services/app/auth-user/helpers/auth-user.helper.ts
@@ -9,7 +9,7 @@ import {
 export const getRegistrationErrorMessage = (
   errorResponse: HttpErrorResponse,
 ): string => {
-  const errorObj = errorResponse.error as RegistrationErrorResponse;
+  const error = errorResponse.error as RegistrationErrorResponse;
 
   const REGISTRATION_ERROR_MAP: Record<RegistrationErrorMessages, string> = {
     EMAIL_EXISTS: 'The email address is already in use by another account.',
@@ -18,18 +18,18 @@ export const getRegistrationErrorMessage = (
       'We have blocked all requests from this device due to unusual activity. Try again later.',
   };
 
-  return REGISTRATION_ERROR_MAP[errorObj.error.message];
+  return REGISTRATION_ERROR_MAP[error.error.message];
 };
 
 export const getLoginErrorMessage = (
   errorResponse: HttpErrorResponse,
 ): string => {
-  const errorObj = errorResponse.error as LoginErrorResponse;
+  const error = errorResponse.error as LoginErrorResponse;
 
   const LOGIN_ERROR_MAP: Record<LoginErrorMessages, string> = {
     INVALID_LOGIN_CREDENTIALS:
       'You have provided invalid credentials. Please, check it and try once again.',
   };
 
-  return LOGIN_ERROR_MAP[errorObj.error.message];
+  return LOGIN_ERROR_MAP[error.error.message];
 };
