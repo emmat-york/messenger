bdf62d2c3769db8fbb2a26a9f64432012dcd65a6
emmat_york <andrei.filimonchyk@gmail.com>
2024-03-17 20:58:06 +0100

    chat: LogOut logic

--- src/app/pages/messenger/components/chat/components/chat-top-bar/chat-top-bar.component.ts
+++ src/app/pages/messenger/components/chat/components/chat-top-bar/chat-top-bar.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { IconPipe } from '../../../../../../shared/pipes/icon/icon.pipe';
+import { AuthUserService } from '../../../../../../shared/services/app/auth-user/auth-user.service';
 
 @Component({
   selector: 'app-chat-top-bar',
@@ -7,6 +9,12 @@ import { NgOptimizedImage } from '@angular/common';
   templateUrl: 'chat-top-bar.component.html',
   styleUrl: 'chat-top-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, IconPipe],
 })
-export class ChatTopBarComponent {}
+export class ChatTopBarComponent {
+  constructor(private readonly authUserService: AuthUserService) {}
+
+  onLogOut(): void {
+    this.authUserService.logOut();
+  }
+}
