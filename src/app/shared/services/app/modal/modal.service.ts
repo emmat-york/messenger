1d247b2221833f6623319565eebd10df7b5db1a3
emmat_york <andrei.filimonchyk@gmail.com>
2024-05-05 21:48:42 +0200

    chat: remove angular cdk and material

--- src/app/shared/services/app/modal/modal.service.ts
+++ src/app/shared/services/app/modal/modal.service.ts
@@ -1,33 +1,6 @@
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Constructor } from '../../../interfaces/common.interface';
-import { Observable } from 'rxjs';
-import { ModalConfig } from './interfaces/modal.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ModalService {
-  constructor(private readonly matDialog: MatDialog) {}
-
-  open<ModalData extends object, CloseAction = undefined>(
-    component: Constructor,
-    modalConfig?: ModalConfig<ModalData>,
-  ): Observable<CloseAction> {
-    const { modalData, settings, multi } = modalConfig || {};
-
-    if (this.matDialog.openDialogs.length && !multi) {
-      this.matDialog.closeAll();
-    }
-
-    const ref = this.matDialog.open(component, settings);
-    const componentRef = ref.componentRef;
-
-    componentRef?.setInput('modalData', modalData ?? {});
-    componentRef?.setInput('closeAction', (action?: CloseAction) =>
-      ref.close(action),
-    );
-
-    return ref.afterClosed();
-  }
-}
+export class ModalService {}
