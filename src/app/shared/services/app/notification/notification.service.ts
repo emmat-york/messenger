41713fa0686aac5a020f66d3fc736d0f289599cd
emmat-york <andrei.filimonchyk@gmail.com>
2024-07-27 18:02:38 +0200

    chat: update 4

--- src/app/shared/services/app/notification/notification.service.ts
+++ src/app/shared/services/app/notification/notification.service.ts
@@ -4,10 +4,6 @@ import {
   Injectable,
   ViewContainerRef,
 } from '@angular/core';
-import {
-  DEFAULT_NOTIFICATION_DURATION,
-  INDENT_BETWEEN_NOTIFICATIONS,
-} from './constants/notification.constant';
 
 @Injectable({
   providedIn: 'root',
@@ -22,68 +18,23 @@ export class NotificationService {
     });
   }
 
-  private get nextElementPosition(): string {
-    return (
-      this.previousElementsTotalHeight +
-      this.modalRefs.length * INDENT_BETWEEN_NOTIFICATIONS +
-      'px'
-    );
+  success(message: string): void {
+    this.openModal({ message, type: 'success' });
   }
 
-  private get previousElementsTotalHeight(): number {
-    return this.modalRefs.reduce((totalHeight, ref, index) => {
-      if (index === this.modalRefs.length - 1) {
-        return totalHeight;
-      }
-
-      return totalHeight + ref.location.nativeElement.offsetHeight;
-    }, 0);
+  warning(message: string): void {
+    this.openModal({ message, type: 'warning' });
   }
 
-  success(message: string, settings?: {}): void {
-    this.openModal({ message, type: 'success', settings });
+  error(message: string): void {
+    this.openModal({ message, type: 'error' });
   }
 
-  warning(message: string, settings?: {}): void {
-    this.openModal({ message, type: 'warning', settings });
-  }
-
-  error(message: string, settings?: {}): void {
-    this.openModal({ message, type: 'error', settings });
-  }
-
-  private openModal({ message, type, settings }: any): void {
-    const ref = this.viewRef.createComponent({} as any);
-    const { timeOut } = settings || {};
-
-    ref.setInput('type', type);
-    ref.setInput('message', message);
-    ref.setInput('closeAction', () => ref.destroy());
-
-    this.modalRefs.push(ref);
-
-    if (this.modalRefs.length > 1) {
-      requestAnimationFrame(() => {
-        const element = ref.location.nativeElement as HTMLElement;
-        element.style.bottom = this.nextElementPosition;
-      });
-    }
-
-    setTimeout(() => {
-      this.destroyModalRef(ref);
-      this.recalculateElementsPosition();
-    }, timeOut ?? DEFAULT_NOTIFICATION_DURATION);
-  }
-
-  private recalculateElementsPosition(): void {
-    this.modalRefs.forEach((ref, index) => {
-      const element = ref.location.nativeElement as HTMLElement;
-      element.style.bottom = index === 0 ? 20 + 'px' : this.nextElementPosition;
-    });
-  }
-
-  private destroyModalRef(modalRef: ComponentRef<any>): void {
-    this.modalRefs = this.modalRefs.filter(ref => ref !== modalRef);
-    modalRef.destroy();
-  }
+  private openModal({
+    message,
+    type,
+  }: {
+    message: string;
+    type: 'success' | 'warning' | 'error';
+  }): void {}
 }
