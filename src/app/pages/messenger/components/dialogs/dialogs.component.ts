5470b5e416cc36931aaf9a28744f29437c8481f1
emmat-york <andrei.filimonchyk@gmail.com>
2024-08-06 22:36:05 +0200

    chat: update 2

--- src/app/pages/messenger/components/dialogs/dialogs.component.ts
+++ src/app/pages/messenger/components/dialogs/dialogs.component.ts
@@ -5,11 +5,8 @@ import {
   Component,
   DestroyRef,
   ElementRef,
-  OnInit,
-  QueryList,
   Renderer2,
   ViewChild,
-  ViewChildren,
 } from '@angular/core';
 import { DialogComponent } from './components/dialog/dialog.component';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
@@ -44,17 +41,14 @@ import { UserMenuModalData } from './components/user-menu/interfaces/user-menu.i
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DialogsComponent implements OnInit, AfterViewInit {
+export class DialogsComponent implements AfterViewInit {
   readonly vm$: Observable<UserState> = this.userFacade.vm$;
   readonly control = new FormControl<string>('', { nonNullable: true });
 
   isScrollCircleShown = false;
-  noSearchResults = false;
 
   @ViewChild('contactList')
   private readonly contactList?: ElementRef<HTMLUListElement>;
-  @ViewChildren(DialogComponent)
-  private readonly contacts?: QueryList<DialogComponent>;
 
   constructor(
     private readonly modalService: ModalService,
@@ -63,18 +57,6 @@ export class DialogsComponent implements OnInit, AfterViewInit {
     private readonly destroyRef: DestroyRef,
     private readonly renderer2: Renderer2,
   ) {}
-
-  ngOnInit(): void {
-    this.control.valueChanges
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(value => {
-        requestAnimationFrame(() => {
-          const hasResult = !!this.contacts?.length;
-          this.noSearchResults = !!value && !hasResult;
-          this.cdRef.markForCheck();
-        });
-      });
-  }
 
   ngAfterViewInit(): void {
     this.subscribeToScroll();
