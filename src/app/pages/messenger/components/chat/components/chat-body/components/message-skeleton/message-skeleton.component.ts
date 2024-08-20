708733913d94d54acf898d08d4b778ba38175718
emmat-york <andrei.filimonchyk@gmail.com>
2024-08-20 22:13:40 +0200

    chat: update 3

--- src/app/pages/messenger/components/chat/components/chat-body/components/message-skeleton/message-skeleton.component.ts
+++ src/app/pages/messenger/components/chat/components/chat-body/components/message-skeleton/message-skeleton.component.ts
@@ -4,11 +4,55 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-message-skeleton',
   standalone: true,
   template: `@for (_ of array(20); track _) {
-    <div class="card is-loading">
-      <div class="image"></div>
+    <div class="outer-card">
+      <div class="inner-card"></div>
     </div>
     } `,
-  styleUrl: 'message-skeleton.component.scss',
+  styles: `:host {
+      display: block;
+      padding: 8px 16px;
+    }
+
+    .outer-card {
+      margin-bottom: 10px;
+      border-radius: 12px;
+      box-shadow: 2px -2px 12px rgb(202, 202, 202);
+
+      &:nth-child(1n) {
+        width: 50%;
+        height: 70px;
+      }
+
+      &:nth-child(2n) {
+        height: 50px;
+        width: 38%;
+      }
+
+      &:nth-child(3n) {
+        height: 76px;
+        width: 26%;
+      }
+
+      &:nth-child(4n) {
+        height: 88px;
+        width: 42%;
+      }
+    }
+
+    .inner-card {
+      height: inherit;
+      border-radius: 12px 12px 12px 0;
+      background: linear-gradient(100deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+      background-size: 200% 100%;
+      animation: 1s shine linear infinite;
+    }
+
+    @keyframes shine {
+      to {
+        background-position-x: -200%;
+      }
+    }
+    `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessageSkeletonComponent {
